ea2f6c949143bb84e2aa940e3e8f75d6b527dd6e
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-11 05:03:25 -0500

    creacion inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-extraboldf">Control Pacientes React</h1>
+    </div>
+  );
+}
+
+export default App;
